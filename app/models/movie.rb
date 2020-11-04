8ffbff80e94ccabc07e4d0bc95b20046052cb4f3
gv <gv2793@gmail.com>
2020-11-04 10:21:50 -0700

    changed logic from executing raw SQL to activerecord methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,6 @@
 class Movie < ApplicationRecord
     def self.find_by_cat(cat)
-        @connection = ActiveRecord::Base.connection
-
-        results = @connection.exec_query(
-            "SELECT * FROM movies WHERE movies.category = '#{cat}'"
-        )
+       results = Movie.where(category: cat)
 
         return results
     end
